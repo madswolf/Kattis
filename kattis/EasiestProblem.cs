535ac52bc7b35ba8971d50de2a1d3a53960ae5a2
Madswolf <mads@madswolf.dk>
2019-10-08 23:47:36 +0200

    made a project and a solution

---
+++ kattis/EasiestProblem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis
+{
+    class EasiestProblem
+    {
+        static void bain(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (!input.Equals("0"))
+            {
+                var inputNumber = int.Parse(input);
+                var sumOfDigits = SumOfDigits(inputNumber);
+
+                for (int i = 11; i < 100000; i++)
+                {
+                    if (SumOfDigits(inputNumber * i) == sumOfDigits)
+                    {
+                        Console.WriteLine(i);
+                        break;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+        }
+
+        public static int SumOfDigits(int input)
+        {
+            var output = 0;
+            int[] digits = Array.ConvertAll(input.ToString().ToCharArray(), c => (int)Char.GetNumericValue(c));
+
+            foreach (int digit in digits)
+            {
+                output += digit;
+            }
+            return output;
+        }
+    }
+}
---
+++ kattis/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis
+{
+    class the
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            while (!input.Equals("0"))
+            {
+                int setSize = int.Parse(input);
+                var set = new List<int>();
+                for (int i = 0; i < setSize; i++)
+                {
+                    set.Add(int.Parse(Console.ReadLine()));   
+                }
+                set = ArrangeSymmetricly(set);
+            }
+        }
+
+        public static List<int> ArrangeSymmetricly(List<int> set)
+        {
+            set.Sort();
+            return set;
+        }
+    }
+}
