649a445bddf89e559219d9976eca66564f8ad6f7
Mads <oeje@itu.dk>
2019-10-09 20:10:38 +0200

    try again

--- kattis/Program.cs
+++ kattis/Program.cs
@@ -8,39 +8,14 @@ using System.Threading.Tasks;
 
 namespace kattis
 {
-    class StackingCups
+    class ReverseBinaryNumber
     {
         static void Main(string[] args)
         {
             var input = Console.ReadLine();
-            while (input != null)
-            {
-                int count = int.Parse(input);
-                List<(int, string)> list = new List<(int, string)>();
-                
-                for (int i = 0; i < count; i++)
-                {
-                    var line = Console.ReadLine().Split(' ');
-
-                    int number;
-                    if (int.TryParse(line[0], out number))
-                    {
-                        list.Add((number/2, line[1]));
-                    }
-                    else
-                    {
-                        number = int.Parse(line[1]);
-                        list.Add((number,line[0]));
-                    }
-                    
-                }
-                list.Sort((x, y) => x.Item1.CompareTo(y.Item1));
-                foreach (var item in list)
-                {
-                    Console.WriteLine(item.Item2);
-                }
-                input = Console.ReadLine();
-            }
+            var binary = Convert.ToString(int.Parse(input),2).ToArray(); 
+            Array.Reverse(binary); 
+            Console.WriteLine(Convert.ToInt32(new String(binary),2));
         }
     }
 }
---
+++ kattis/StackingCups.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kattis
+{
+    class StackingCups
+    {
+        static void bain(string[] args)
+        {
+            var input = Console.ReadLine();
+            while (input != null)
+            {
+                int count = int.Parse(input);
+                List<(int, string)> list = new List<(int, string)>();
+                
+                for (int i = 0; i < count; i++)
+                {
+                    var line = Console.ReadLine().Split(' ');
+
+                    int number;
+                    if (int.TryParse(line[0], out number))
+                    {
+                        list.Add((number/2, line[1]));
+                    }
+                    else
+                    {
+                        number = int.Parse(line[1]);
+                        list.Add((number,line[0]));
+                    }
+                    
+                }
+                list.Sort((x, y) => x.Item1.CompareTo(y.Item1));
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item.Item2);
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
