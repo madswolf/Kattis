8b9822e731d3ceb905ae129807b2a09b0470e926
Madswolf <mads@madswolf.dk>
2020-03-08 00:21:43 +0100

    virus but too slow

--- F#/virus/Program.fs
+++ F#/virus/Program.fs
@@ -8,11 +8,20 @@ let main argv =
     let original = Console.ReadLine().ToCharArray()
     let replication = Console.ReadLine().ToCharArray()
     let length = min original.Length replication.Length
-    let roundone = List.fold (fun acc item -> if original.[item] <> replication.[item] then (if acc = -1 then item else acc) else acc ) -1 [0..(length - 1)]
-    printfn "%i" roundone
-    let original = Array.rev original.[roundone..]
-    let replication = Array.rev replication.[roundone..]
-    printfn "%A %A" original replication
-    let length = min original.Length replication.Length
-    //let roundtwo = List.fold () (0,-1) [0..length]
+
+    let func (c:char [] * char [] * int) =
+        let list1,list2,status = c
+        if status <> -1 
+        then (list1,list2,status)
+        else 
+            if list1.Length <> 0 && list2.Length <> 0 && list1.[0] = list2.[0]
+            then (list1.[1..],list2.[1..],status)
+            else (list1,list2,0)
+
+    let roundone = List.fold (fun acc item -> func acc) (original,replication,-1) [0..(length-1)] 
+    let original,replication,_ = roundone
+    let roundtwo = List.fold (fun acc item -> func acc) (Array.rev original,Array.rev replication,-1) [0..(length-1)]
+    
+    let _,replication,_ = roundtwo
+    printfn "%A" replication.Length
     0 // return an integer exit code
